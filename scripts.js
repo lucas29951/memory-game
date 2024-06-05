5d6541fdd73c52d9b0bd83a1b78ce6da3ad195b4
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-05 20:34:42 -0300

    Se definieron las variables a utilizar

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+let tarjetasDestapadas = 0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+let primerResultado = null;
+let segundoResultado = null;
+let movimientos = 0;
+let aciertos = 0;
+let temporizador = false;
+let timer = 30;
+let timerInicial = 30;
+let tiempoRegresivoId = null;
+
