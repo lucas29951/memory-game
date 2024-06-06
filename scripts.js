bbef218b4ace56d607a2b87c43bcaeaee8f7065e
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-06 12:39:29 -0300

    Se agrego la funcion para contar el tiempo de juego

--- scripts.js
+++ scripts.js
@@ -27,3 +27,16 @@ let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 numeros = numeros.sort(() => {
     return Math.random() - 0.5;
 });
+
+
+function contarTiempo() {
+    tiempoRegresivoId = setInterval(() => {
+        timer--;
+        mostrartiempo.innerHTML = `Tiempo: ${timer} segundos`;
+        if(timer == 0) {
+            clearInterval(tiempoRegresivoId);
+            bloquearTarjetas();
+            loseAudio.play();
+        }
+    }, 1000);
+}
